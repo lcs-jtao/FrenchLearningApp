94be1ab3d481a130bd087f734476d00d09c179dd
Joyce Tao <jtao@lcs.on.ca>
2021-12-07 15:27:18 -0500

    Added a few French phrases and their English translation, and defined the structure

--- FrenchLearningApp/Model/Card.swift
+++ FrenchLearningApp/Model/Card.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct Card {
+    let frenchPhrase: String
+    let englishTranslation: String
+}
+
+let listOfWords = [
+
+    Card(frenchPhrase: "Bonjour!",
+         englishTranslation: "Hello!")
+    ,
+    Card(frenchPhrase: "Je m'appelle...",
+         englishTranslation: "My name is...")
+    ,
+    Card(frenchPhrase: "Comment ça va?",
+         englishTranslation: "How are you?")
+    ,
+    Card(frenchPhrase: "Ça va bien, toi?",
+         englishTranslation: "I'm good, how about you?")
+    ,
+    Card(frenchPhrase: "Au revoir!",
+         englishTranslation: "Goodbye!")
+    
+]
