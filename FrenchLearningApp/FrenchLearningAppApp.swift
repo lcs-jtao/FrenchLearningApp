7c87497b41855ab803e214b406388c4e830fd282
Joyce Tao <jtao@lcs.on.ca>
2021-12-07 15:17:29 -0500

    Initial Commit

---
+++ FrenchLearningApp/FrenchLearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrenchLearningAppApp.swift
+//  FrenchLearningApp
+//
+//  Created by Joyce Tao on 2021-12-07.
+//
+
+import SwiftUI
+
+@main
+struct FrenchLearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
